ff2f0710bfd2cb6286a182b2e25f8bd9674ac867
Matthew Derrick <me@mattderrick.co.uk>
2024-06-27 10:40:36 +0100

    minor: add three

--- packages/add/index.ts
+++ packages/add/index.ts
@@ -1,3 +1,4 @@
 export const add = (a: number, b: number) => a + b;
 export const addOne = (a: number) => add(a, 1);
 export const addTwo = (a: number) => add(a, 2);
+export const addThree = (a: number) => add(a, 3);
