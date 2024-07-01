ac4e11010b1cd313540299c8f91fde231863ad71
Matthew Derrick <me@mattderrick.co.uk>
2024-07-01 10:51:49 +0100

    feat: add 7

--- packages/add/index.ts
+++ packages/add/index.ts
@@ -5,3 +5,4 @@ export const addThree = (a: number) => add(a, 3);
 export const addFour = (a: number) => add(a, 4);
 export const addFive = (a: number) => add(a, 5);
 export const addSix = (a: number) => add(a, 6);
+export const addSeven = (a: number) => add(a, 7);
