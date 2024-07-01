5ba3a5ef6560225d65ca2e874a4284a43bdd769b
Matthew Derrick <me@mattderrick.co.uk>
2024-07-01 10:57:30 +0100

    feat: subtract 3

--- packages/subtract/index.ts
+++ packages/subtract/index.ts
@@ -1,3 +1,4 @@
 export const subtract = (a: number, b: number) => a - b;
 export const subtractOne = (a: number) => subtract(a, 1);
 export const subtractTwo = (a: number) => subtract(a, 2);
+export const subtractThree = (a: number) => subtract(a, 3);
